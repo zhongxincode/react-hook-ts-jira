34fadbbca1b4881c89a812b16de5181e23156a41
zx <3313147896@qq.com>
2021-05-01 22:56:58 +0800

    feat: 使用json-server的middleware模拟非restful请求

---
+++ __json_server_mock__/middleware.js
@@ -0,0 +1,14 @@
+module.exports = (req, res, next) => {
+  if (req.method === "POST" && req.path === "/login") {
+    if (req.body.username === "jira" && req.body.password === "123") {
+      return res.status(200).json({
+        user: {
+          token: "1z2d",
+        },
+      });
+    } else {
+      return res.status(400).json({ message: "用户名或者密码错误" });
+    }
+  }
+  next();
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import { Login } from './pages/login';
 import { ProjectList } from './pages/project-list';
 
 function App() {
   return (
     <div className="App">
-      <ProjectList/>
+      <Login/>
+      {/* <ProjectList/> */}
     </div>
   );
 }
