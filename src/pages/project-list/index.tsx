81522eb9b5218544dbb2ca1cf9c34376a161e9bf
zx <3313147896@qq.com>
2021-05-03 23:45:48 +0800

    feat: 完成URL的状态管理

--- src/pages/project-list/index.tsx
+++ src/pages/project-list/index.tsx
@@ -1,17 +1,19 @@
 import styled from "@emotion/styled";
-import { useState } from "react";
 import { useProjects } from "../../utils/project";
 import { useDebounce, useDocumentTitle } from "../../utils";
 import { List } from "./list";
 import { SearchPanel } from "./search-panel";
 import { useUsers } from "../../utils/user";
+import { useUrlQueryParam } from "../../utils/url";
+
+// 状态提升
+
+// 基本类型，可以放到依赖里；组件状态，可以放到依赖里；非组件状态的对象，绝不可以放到依赖里
+// https://codesandbox.io/s/keen-wave-tlz9s?file=/src/App.js
 
 export const ProjectList = () => {
-  // 状态提升
-  const [param, setParam] = useState({
-    name: "",
-    personId: "",
-  });
+
+  const [param, setParam] = useUrlQueryParam(['name', 'personId']);
 
   const debouncedParam = useDebounce(param, 200);
 
@@ -28,6 +30,8 @@ export const ProjectList = () => {
     </Container>
   );
 };
+
+ProjectList.whyDidYouRender = true;
 
 const Container = styled.div`
   padding: 3.2rem;
