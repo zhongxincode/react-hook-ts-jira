53b99a7f955454ed749814718e49002407e71c9a
zx <3313147896@qq.com>
2021-05-05 09:35:25 +0800

    feat: 阻止在已卸载组件上赋值

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -92,3 +92,19 @@ export const useDocumentTitle = (title: string, keepOnUnmount = true) => {
 };
 
 export const resetRoute = () => window.location.href = window.location.origin
+
+/**
+ * 返回组件的挂载状态，如果还没挂载或者已经卸载，返回false；反之，返回true
+ */
+ export const useMountedRef = () => {
+  const mountedRef = useRef(false);
+
+  useEffect(() => {
+    mountedRef.current = true;
+    return () => {
+      mountedRef.current = false;
+    };
+  });
+
+  return mountedRef;
+};
